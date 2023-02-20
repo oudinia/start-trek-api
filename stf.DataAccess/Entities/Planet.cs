bc36e1c09a72df28ceb568dc9674ff4d62dc9ea6
Oussama Dinia <oussama.dinia@gmail.com>
2023-02-20 13:59:13 +0100

    Add project files.

---
+++ stf.DataAccess/Entities/Planet.cs
@@ -0,0 +1,8 @@
+﻿namespace stf.DataAccess.Entities;
+
+public class Planet
+{
+    public int PlanetId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ stf.DataAccess/StarFleetDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using stf.DataAccess.Entities;
+
+namespace stf.DataAccess
+{
+    public class StarFleetDbContext: DbContext
+    {
+        public DbSet<Planet> Planets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=AdventureWorks2019;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+
+}
